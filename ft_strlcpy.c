4c20b2b5d5dc89a52ba9c2c920fb2f6bb44aed6b
Haddani Hamza <hhamza@e2r6p9.1337.ma>
2021-11-03 11:24:25 +0100

    Updated ft_strlcpy's header

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,18 +6,18 @@
 /*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 09:44:38 by hhamza            #+#    #+#             */
-/*   Updated: 2021/11/03 11:09:25 by hhamza           ###   ########.fr       */
+/*   Updated: 2021/11/03 11:23:51 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dst_size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	int	i;
 
 	i = 0;
-	while ((dst_size--) - 1)
+	while ((dstsize--) - 1)
 		*(dst++) = src[i++];
 	*dst = 0;
 	return (ft_strlen(src));
