c74aab6c136eb37c50bb30c98e2da37249363279
Haddani Hamza <hhamza@e2r5p8.1337.ma>
2021-11-04 21:57:58 +0100

    Added ft_memmove

---
+++ ft_memmove.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/01 18:26:08 by hhamza            #+#    #+#             */
+/*   Updated: 2021/11/04 21:57:16 by hhamza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+#include <stdio.h>
+
+static void	*ft_memmove_overlap(void *dst, const void *src, size_t len)
+{
+	size_t	i;
+
+	i = len;
+	while (--i > 0)
+		((char *) dst)[i] = ((char *) src)[i];
+	((char *) dst)[i] = ((char *) src)[i];
+	return (dst);
+}
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	if (src - dst < 0)
+		return (ft_memmove_overlap(dst, src, len));
+	else
+		return (ft_memcpy(dst, src, len));
+}
