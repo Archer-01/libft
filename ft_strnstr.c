ca546d8f2dd176e3039ef4b732ad7d1ef0a1047b
Haddani Hamza <hhamza@e2r5p8.1337.ma>
2021-11-04 15:08:19 +0100

    Added ft_strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 14:54:15 by hhamza            #+#    #+#             */
+/*   Updated: 2021/11/04 13:02:44 by hhamza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	ft_needle(const char *haystack, const char *needle, size_t len,
+	int i)
+{
+	size_t	count;
+	size_t	j;
+
+	count = 0;
+	j = -1;
+	while (needle[++j] && i + j < len)
+	{
+		if (haystack[i + j] == needle[j])
+			++count;
+		else
+			count = 0;
+	}
+	return (count);
+}
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+
+	if (!needle)
+		return ((char *) haystack);
+	i = -1;
+	while (++i < len && haystack[i])
+	{
+		if (haystack[i] == needle[0])
+		{
+			if (ft_needle(haystack, needle, len, i) == ft_strlen(needle))
+				return ((char *)(haystack + i));
+		}
+	}
+	return (0);
+}
