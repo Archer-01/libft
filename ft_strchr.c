e099b5684c995d6a3443e750704135e54e3f189e
Haddani Hamza <hhamza@e1r9p1.1337.ma>
2021-11-05 17:48:54 +0100

    Fixed ft_strchr (Typecasted c as char to treat char overflows)

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 13:05:19 by hhamza            #+#    #+#             */
-/*   Updated: 2021/11/03 13:16:04 by hhamza           ###   ########.fr       */
+/*   Updated: 2021/11/05 17:27:32 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s && *s != c)
+	while (*s && *s != (char) c)
 		++s;
-	if (*s == c)
+	if (*s == (char) c)
 		return ((char *) s);
 	return (0);
 }
