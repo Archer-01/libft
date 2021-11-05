9a9fdf2f7628784b380e0c8b95a3ea439802f066
Haddani Hamza <hhamza@e1r6p4.1337.ma>
2021-11-05 21:59:47 +0100

    Added ft_substr

---
+++ ft_substr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/05 21:01:36 by hhamza            #+#    #+#             */
+/*   Updated: 2021/11/05 21:57:14 by hhamza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	i;
+
+	substr = (char *) malloc(sizeof(char) * (len + 1));
+	if (!substr)
+		return (0);
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		substr[i] = s[start + i];
+		++i;
+	}
+	substr[i]= 0;
+	return (substr);
+}
