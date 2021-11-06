1ff4a64792b3719392090e5ed054cdd67cc9b138
Haddani Hamza <hhamza@e1r2p13.1337.ma>
2021-11-06 11:22:38 +0100

    Fixed ft_strlcat (Case when dstlen == 0 & Fixed the main loop condition; dstsize-- - dstlen - 1 && *src)

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 11:25:08 by hhamza            #+#    #+#             */
-/*   Updated: 2021/11/05 16:57:08 by hhamza           ###   ########.fr       */
+/*   Updated: 2021/11/06 11:14:12 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,17 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 
 	srclen = ft_strlen(src);
 	dstlen = ft_strlen(dst);
-	if (dstsize <= srclen)
-		return (srclen + dstsize);
-	while (*dst)
-		++dst;
-	while ((dstsize--) - srclen - 1 != 0)
-		*(dst++) = *(src++);
-	*dst = 0;
+	if (dstlen != 0)
+	{
+		if (dstsize <= srclen)
+			return (srclen + dstsize);
+		while (*dst)
+			++dst;
+		while ((dstsize--) - dstlen - 1 != 0 && *src)
+			*(dst++) = *(src++);
+		*dst = 0;
+	}
+	else
+		ft_strlcpy(dst, src, srclen + 1);
 	return (srclen + dstlen);
 }
