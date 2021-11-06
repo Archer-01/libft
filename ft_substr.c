ccd388e21ae5f22bd602c3898b9268380b649e9e
Haddani Hamza <hhamza@e1r2p13.1337.ma>
2021-11-06 10:04:35 +0100

    Fixed a Norm Error on ft_substr & Made code shorter (++i inside the while loop)

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 21:01:36 by hhamza            #+#    #+#             */
-/*   Updated: 2021/11/05 21:57:14 by hhamza           ###   ########.fr       */
+/*   Updated: 2021/11/06 10:03:23 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	substr = (char *) malloc(sizeof(char) * (len + 1));
 	if (!substr)
 		return (0);
-	i = 0;
-	while (i < len && s[start + i])
-	{
+	i = -1;
+	while (++i < len && s[start + i])
 		substr[i] = s[start + i];
-		++i;
-	}
-	substr[i]= 0;
+	substr[i] = 0;
 	return (substr);
 }
