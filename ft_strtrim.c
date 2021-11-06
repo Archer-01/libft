9afb304c6d2bf682e1c1b7b69d69f6674726aae7
Haddani Hamza <hhamza@e1r2p13.1337.ma>
2021-11-06 12:15:07 +0100

    Added ft_strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/06 11:29:36 by hhamza            #+#    #+#             */
+/*   Updated: 2021/11/06 11:57:04 by hhamza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_isset(char const c, char const *set)
+{
+	while (*set)
+		if (c == *(set++))
+			return (1);
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	unsigned int	start;
+	size_t			end;
+
+	start = 0;
+	while (ft_isset(s1[start], set) && s1[start])
+		++start;
+	end = ft_strlen(s1) - 1;
+	while (end > start && ft_isset(s1[end], set))
+		--end;
+	return (ft_substr(s1, start, end - start + 1));
+}
