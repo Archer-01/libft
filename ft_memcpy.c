da43f18d6dc8878184891dbba22bc7ec6f3919bf
Haddani Hamza <hhamza@e1r2p13.1337.ma>
2021-11-06 17:55:03 +0100

    Fixed ft_memcpy (dst & src both set to 0)

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 17:22:30 by hhamza            #+#    #+#             */
-/*   Updated: 2021/11/02 17:58:09 by hhamza           ###   ########.fr       */
+/*   Updated: 2021/11/06 17:44:58 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
 
+	if (!src && !dst)
+		return (0);
 	i = -1;
 	while (++i < n)
 		((char *) dst)[i] = ((char *) src)[i];
