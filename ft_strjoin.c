70f6d83c9f40cf592a64e1c1deb12015008d37e4
Haddani Hamza <hhamza@e1r2p13.1337.ma>
2021-11-06 11:26:53 +0100

    Added ft_strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/06 10:07:37 by hhamza            #+#    #+#             */
+/*   Updated: 2021/11/06 11:20:23 by hhamza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*res;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	res = (char *) malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (!res)
+		return (0);
+	*res = 0;
+	ft_strlcpy(res, s1, s1_len + 1);
+	ft_strlcat(res, s2, s1_len + s2_len + 1);
+	return (res);
+}
