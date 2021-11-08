8771fb458d89cd3449f6de07e12665f38d7ea482
Haddani Hamza <hhamza@e1r3p8.1337.ma>
2021-11-08 13:45:22 +0100

    Fixed ft_strnstr (Changed '!needle' to '!(*needle)')

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 14:54:15 by hhamza            #+#    #+#             */
-/*   Updated: 2021/11/05 19:07:23 by hhamza           ###   ########.fr       */
+/*   Updated: 2021/11/08 13:42:28 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,29 +21,25 @@ static size_t	ft_needle(const char *haystack, const char *needle, size_t len,
 	count = 0;
 	j = -1;
 	while (needle[++j] && i + j < len)
-	{
 		if (haystack[i + j] == needle[j])
 			++count;
-		else
-			count = 0;
-	}
 	return (count);
 }
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
-	size_t	needlelen;
+	// size_t	needlelen;
 
-	needlelen = ft_strlen(needle);
-	if (!needle)
+	// needlelen = ft_strlen(needle);
+	if (!(*needle))
 		return ((char *) haystack);
 	i = -1;
 	while (++i < len && haystack[i])
 	{
 		if (haystack[i] == needle[0])
 		{
-			if (ft_needle(haystack, needle, len, i) == needlelen)
+			if (ft_needle(haystack, needle, len, i) == ft_strlen(needle))
 				return ((char *)(haystack + i));
 		}
 	}
