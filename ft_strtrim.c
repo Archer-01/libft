ae9abf2219995efd8a874157e530fd465da243f1
Haddani Hamza <hhamza@e1r9p9.1337.ma>
2021-11-08 18:38:32 +0100

    Fixed ft_strtrim (Protected s1 & set)

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 11:29:36 by hhamza            #+#    #+#             */
-/*   Updated: 2021/11/06 11:57:04 by hhamza           ###   ########.fr       */
+/*   Updated: 2021/11/08 18:26:41 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ char	*ft_strtrim(char const *s1, char const *set)
 	unsigned int	start;
 	size_t			end;
 
+	if (!s1 || !set)
+		return (0);
 	start = 0;
 	while (ft_isset(s1[start], set) && s1[start])
 		++start;
