6048d67510a4e5291632f729e979d9fc6c02c83f
Haddani Hamza <hhamza@e1r3p8.1337.ma>
2021-11-08 11:14:13 +0100

    Fixed ft_memcmp (Avoided reading too many bytes from s1 & s2)

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 14:13:12 by hhamza            #+#    #+#             */
-/*   Updated: 2021/11/05 21:18:27 by hhamza           ###   ########.fr       */
+/*   Updated: 2021/11/08 11:12:56 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,12 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	if (!n)
-		return (0);
-	while (n - 1 && *((unsigned char *) s1++) == *((unsigned char *) s2++))
-		--n;
-	return (*((unsigned char *) s1) - *((unsigned char *) s2));
+	while (n--)
+	{
+		if (*((unsigned char *) s1) != *((unsigned char *) s2))
+			return (*((unsigned char *) s1) - *((unsigned char *) s2));
+		++s1;
+		++s2;
+	}
+	return (0);
 }
