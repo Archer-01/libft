f6ec71e3b6aabfc0363e79678e86be0ef6e27141
Haddani Hamza <hhamza@e1r7p14.1337.ma>
2021-11-09 13:13:15 +0100

    Added ft_itoa

---
+++ ft_itoa.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/09 09:53:19 by hhamza            #+#    #+#             */
+/*   Updated: 2021/11/09 13:12:07 by hhamza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_digitcount(int n)
+{
+	int	digitcount;
+
+	if (n == 0)
+		return (1);
+	digitcount = 0;
+	if (n < 0)
+		++digitcount;
+	while (n != 0)
+	{
+		++digitcount;
+		n /= 10;
+	}
+	return (digitcount);
+}
+
+char	*ft_itoa(int n)
+{
+	int		digitcount;
+	char	*res;
+
+	digitcount = ft_digitcount(n);
+	res = (char *) malloc(sizeof(char) * (digitcount + 1));
+	if (!res)
+		return (0);
+	res[digitcount] = 0;
+	if (n < 0)
+		res[0] = '-';
+	else if (n == 0)
+		res[0] = '0';
+	while (n != 0)
+	{
+		if (n == -2147483648)
+			res[--digitcount] = '8';
+		else
+		{
+			if (n < 0)
+				n *= -1;
+			res[--digitcount] = (n % 10) + '0';
+		}
+		n /= 10;
+	}
+	return (res);
+}
