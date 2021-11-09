105ba869e132f055293af8b668a64dc344886048
Haddani Hamza <hhamza@e1r7p14.1337.ma>
2021-11-09 15:00:44 +0100

    Fixed ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 13:47:58 by hhamza            #+#    #+#             */
-/*   Updated: 2021/11/08 11:38:43 by hhamza           ###   ########.fr       */
+/*   Updated: 2021/11/09 14:41:35 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,12 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	unsigned int	i;
+
 	if (n == 0)
 		return (0);
-	while ((*s1 && *s2) && n--)
-	{
-		if ((unsigned char) *s1 != (unsigned char) *s2)
-			return ((unsigned char) *s1 - (unsigned char) *s2);
-		++s1;
-		++s2;
-	}
-	if (n == 0)
-		return (0);
-	return ((unsigned char) *s1 - (unsigned char) *s2);
+	i = 0;
+	while ((s1[i] && s2[i]) && s1[i] == s2[i] && i < n - 1)
+		++i;
+	return ((unsigned char) s1[i] - (unsigned char) s2[i]);
 }
