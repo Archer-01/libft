1e7474ca1d5572163207379fc60c049fde19203b
Haddani Hamza <hhamza@e1r7p14.1337.ma>
2021-11-09 13:40:07 +0100

    Added ft_strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/09 13:23:33 by hhamza            #+#    #+#             */
+/*   Updated: 2021/11/09 13:39:11 by hhamza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*res;
+	unsigned int	i;
+
+	if (!s || !f)
+		return (0);
+	res = (char *) malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!res)
+		return (0);
+	i = -1;
+	while (s[++i])
+		res[i] = (*f)(i, s[i]);
+	res[i] = 0;
+	return (res);
+}
