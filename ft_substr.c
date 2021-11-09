de5a20fe4fb97f6dafe5df18d3d04654ba67fb2b
Haddani Hamza <hhamza@e1r7p14.1337.ma>
2021-11-09 15:23:55 +0100

    Fixed ft_substr (Memory leak; start + len > slen)

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 21:01:36 by hhamza            #+#    #+#             */
-/*   Updated: 2021/11/08 18:04:29 by hhamza           ###   ########.fr       */
+/*   Updated: 2021/11/09 15:22:45 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,24 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
+	size_t	slen;
 
 	if (!s)
 		return (0);
-	substr = (char *) malloc(sizeof(char) * (len + 1));
-	if (!substr)
-		return (0);
-	if (start >= ft_strlen(s))
+	slen = ft_strlen(s);
+	if (start > slen)
+	{
+		substr = (char *) malloc(sizeof(char));
 		*substr = 0;
+	}
 	else
+	{
+		if (start + len > slen)
+			len = slen;
+		substr = (char *) malloc(sizeof(char) * (len + 1));
+		if (!substr)
+			return (0);
 		ft_strlcpy(substr, s + start, len + 1);
+	}
 	return (substr);
 }
