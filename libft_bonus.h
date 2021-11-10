ca8f2de078753b80d598fbee70b090c55b82619a
Haddani Hamza <hhamza@e1r5p12.1337.ma>
2021-11-10 22:52:40 +0100

    Updated .h files (Moved s_list to libft_bonus.h)

--- libft_bonus.h
+++ libft_bonus.h
@@ -6,7 +6,7 @@
 /*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 11:38:25 by hhamza            #+#    #+#             */
-/*   Updated: 2021/11/10 21:37:10 by hhamza           ###   ########.fr       */
+/*   Updated: 2021/11/10 22:51:23 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,7 @@
 # define LIBFT_BONUS_H
 
 # include <stdlib.h>
-
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}	t_list;
+# include "libft.h"
 
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
