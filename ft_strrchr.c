a432a36e8d12d42e88c57017775b19f5ecd2aaf1
Haddani Hamza <hhamza@e1r8p7.1337.ma>
2021-11-12 01:10:20 +0100

    Fixed ft_strrchr (Optimized code)

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 13:23:05 by hhamza            #+#    #+#             */
-/*   Updated: 2021/11/08 11:24:27 by hhamza           ###   ########.fr       */
+/*   Updated: 2021/11/12 01:04:19 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,9 @@ char	*ft_strrchr(const char *s, int c)
 	size_t	len;
 
 	len = ft_strlen(s);
-	if (s[len] == 0 && (char) c == 0)
-		return ((char *) s + len);
-	while (len > 0 && s[len - 1] != (char) c)
+	while (len > 0 && s[len] != (char) c)
 		--len;
-	if (s[len - 1] == (char) c)
-		return ((char *)(s + len - 1));
+	if (s[len] == (char) c)
+		return ((char *)(s + len));
 	return (0);
 }
