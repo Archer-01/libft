471ddec425ad725ad4f07e8a7f5c23200c9d8afe
Haddani Hamza <hhamza@e1r8p7.1337.ma>
2021-11-12 02:33:56 +0100

    Fixed ft_strdup (Reverted back to original malloc size)

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 17:28:30 by hhamza            #+#    #+#             */
-/*   Updated: 2021/11/12 02:03:07 by hhamza           ###   ########.fr       */
+/*   Updated: 2021/11/12 02:33:27 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ char	*ft_strdup(const char *s1)
 	size_t	s1_len;
 
 	s1_len = ft_strlen(s1) + 1;
-	s2 = (char *) malloc((s1_len + 1) * sizeof(char));
+	s2 = (char *) malloc((s1_len) * sizeof(char));
 	if (!s2)
 		return (0);
-	ft_strlcpy(s2, s1, s1_len + 1);
+	ft_strlcpy(s2, s1, s1_len);
 	return (s2);
 }
