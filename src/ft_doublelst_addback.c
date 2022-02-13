452f8e89731f83df5e8fb6f87b39fd90b4db2c9f
Hamza Haddani <hhamza@student.1337.ma>
2022-02-13 13:48:26 +0100

    Fix ft_doublelst_addback
    Add NULL param protection to ft_doublelst_addback.

--- src/ft_doublelst_addback.c
+++ src/ft_doublelst_addback.c
@@ -6,7 +6,7 @@
 /*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 10:06:33 by hhamza            #+#    #+#             */
-/*   Updated: 2022/02/09 10:37:32 by hhamza           ###   ########.fr       */
+/*   Updated: 2022/02/13 13:44:42 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ void	ft_doublelst_addback(t_double_list **lst, t_double_list *new)
 	t_double_list	*old_last;
 	t_double_list	*new_last;
 
+	if (lst == NULL || new == NULL)
+		return ;
 	old_last = ft_doublelst_last(*lst);
 	ft_lstadd_back((t_list **) lst, (t_list *) new);
 	new_last = ft_doublelst_last(*lst);
